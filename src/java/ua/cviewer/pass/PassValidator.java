ca452b3afea5a0b4fbc394ee0cf12a89006eee65
Ivan <kurinniy_i@ukr.net>
2014-07-19 17:57:11 +0300

    Pass

--- src/java/ua/cviewer/pass/PassValidator.java
+++ src/java/ua/cviewer/pass/PassValidator.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ua.cviewer.pass;
 
 /**
@@ -12,4 +6,5 @@ package ua.cviewer.pass;
  */
 public interface PassValidator {
     
+    boolean checkPass(int id, long pass);
 }
